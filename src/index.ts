d46e9b8a662922f62754309beac14570c2381116
Tally <113806118+thenatally@users.noreply.github.com>
2025-06-15 21:31:17 -0400

    change port so health check fails

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,6 @@ http.createServer((req, res) => {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not Found");
   }
-}).listen(3000, () => {
+}).listen(3001, () => {
   console.log("HTTP server listening on port 3000");
 });
